73272ddc613dec67fdf644de350340d16b212a97
Samuel <JS.Rosemond@gmail.com>
2023-02-17 14:17:39 -0500

    change router setup from create to jsx routes

--- src/router/RouterSetup.jsx
+++ src/router/RouterSetup.jsx
@@ -3,14 +3,17 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  Link,
+  Routes,
   Outlet,
 } from "react-router-dom";
 import App from "../app/App";
+import Reservations from "../pages/BookingFormPage/Reservations";
 import Navbar from "../components/Navigation/Navbar";
+import Home from "../pages/HomePage/Home";
 import About from "../pages/AboutPage/About";
+import OnlineMenu from "../pages/OnlineMenu/OnlineMenu";
 
-export function Navigation() {
+function Navigation() {
   return (
     <>
       <Navbar />
@@ -19,6 +22,7 @@ export function Navigation() {
   );
 }
 
+// buggy: left for ref
 const router = createBrowserRouter([
   {
     element: <Navigation />,
@@ -28,11 +32,33 @@ const router = createBrowserRouter([
         element: <App />,
       },
       {
+        path: "menu",
+        element: <OnlineMenu />,
+      },
+      {
+        path: "reservations",
+        element: <Reservations />,
+      },
+      {
         path: "about",
         element: <About />,
       },
     ],
   },
 ]);
+
+// used
+export function NavigationRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Home />} />
+        <Route path="menu" element={<OnlineMenu />} />
+        <Route path="reservations" element={<Reservations />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
+  );
+}
 
 export default router;
