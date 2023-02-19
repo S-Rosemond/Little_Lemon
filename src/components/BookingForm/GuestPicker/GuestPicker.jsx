637e555571fab34349b80354b0ebeba394ab6a1c
Samuel <JS.Rosemond@gmail.com>
2023-02-19 18:10:49 -0500

    Guest Picker updated (helper text)

--- src/components/BookingForm/GuestPicker/GuestPicker.jsx
+++ src/components/BookingForm/GuestPicker/GuestPicker.jsx
@@ -1,21 +1,31 @@
-import { Input, Select } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Select,
+} from "@chakra-ui/react";
 import React from "react";
 import { CreateSelectNumber } from "../TimePicker/TimePickerOptions";
 
 function GuestPicker({ formik }) {
   return (
     <>
-      <label htmlFor="guests">Number of guests</label>
-      <Select
-        name="guests"
-        type="number"
-        min="1"
-        max="10"
-        id="guests"
-        {...formik.getFieldProps("guests")}
-      >
-        <CreateSelectNumber number={10} />
-      </Select>
+      <FormControl>
+        <FormLabel htmlFor="guests">Number of guests</FormLabel>
+        <Select
+          name="guests"
+          type="number"
+          min="1"
+          max="10"
+          id="guests"
+          {...formik.getFieldProps("guests")}
+        >
+          <CreateSelectNumber number={10} />
+        </Select>
+        <FormHelperText>
+          Please select the number of guest for the occasion
+        </FormHelperText>
+      </FormControl>
     </>
   );
 }
